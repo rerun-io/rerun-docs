a3d2b4214f39b43b59ea52826a023b591b21a4f5
Andreas Reich <r_andreas2@web.de>
2023-05-25 11:59:33 +0200

    fixup code_concat

--- code_concat.py
+++ code_concat.py
@@ -4,32 +4,34 @@ Utility script for concating all code sections in a markdown file into a single 
 
 
 import os
-#import subprocess
+
+# import subprocess
 
 
 md_files_with_python = [
-    'docs/getting-started/logging-python.md',
-    'docs/getting-started/python.md'
+    "docs/getting-started/logging-python.md",
+    "docs/getting-started/python.md",
 ]
-md_files_with_rust = [
-    'docs/getting-started/logging-rust.md'
-]
+md_files_with_rust = ["docs/getting-started/logging-rust.md"]
 
-def extract_strings_between(text: str, start_marker:str , end_marker: str, comment_chars: str) -> str:
+
+def extract_strings_between(
+    text: str, start_marker: str, end_marker: str, comment_chars: str
+) -> str:
     result = ""
     start_index = text.find(start_marker)
-    end_index = text.find(end_marker)
-    
+    end_index = text.find(end_marker, start_index + len(start_marker))
+
     while start_index != -1 and end_index != -1:
         result += "\n\n" + comment_chars + " ------------------------------\n\n"
-        result += text[start_index + len(start_marker):end_index] + "\n"
+        result += text[start_index + len(start_marker) : end_index] + "\n"
         start_index = text.find(start_marker, end_index + len(end_marker))
         end_index = text.find(end_marker, start_index + len(start_marker))
-    
+
     return result
 
 
-def write_code(code: str, origin_file_path:str, extension: str) -> str:
+def write_code(code: str, origin_file_path: str, extension: str) -> str:
     folder = ".concated_code"
 
     if not os.path.exists(folder):
@@ -46,11 +48,11 @@ def write_code(code: str, origin_file_path:str, extension: str) -> str:
 
 
 for path in md_files_with_python:
-    with open(path, 'r') as file:
+    with open(path, "r") as file:
         contents = file.read()
     python_code = extract_strings_between(contents, "```python", "```", "#")
     code_file = write_code(python_code, path, "py")
-    
+
     # print("Running ", code_file)
     # result = subprocess.run(["python", code_file], capture_output=True, text=True)
     # print(result.stdout)
@@ -58,7 +60,7 @@ for path in md_files_with_python:
     # print(result.returncode)
 
 for path in md_files_with_rust:
-    with open(path, 'r') as file:
+    with open(path, "r") as file:
         contents = file.read()
     rust_code = extract_strings_between(contents, "```rust", "```", "//")
     code_file = write_code(rust_code, path, "rs")
